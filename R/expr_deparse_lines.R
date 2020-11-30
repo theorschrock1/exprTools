8d5c11aa5adfe4d819f50cb6e317b5e0ce458587
theordore schrock <theorschrock@gmail.com>
2020-11-30 14:30:23 -0700

    added expr_deparse_lines.R

---
+++ R/expr_deparse_lines.R
@@ -0,0 +1,22 @@
+#' deparse multiple lines of R code.
+
+#' @name expr_deparse_lines
+#' @param x  \code{[call]}  Must be a call from function \code{'{'}.
+#' @return \code{expr_deparse_lines}: [character]
+#' @examples
+
+#'  expr_deparse_lines({
+#'  sq <- function(x) {
+#'  x^2
+#'  }
+#'  sq(2)
+#'  sq(4)
+#'  })
+#' @export
+expr_deparse_lines <- function(x) {
+    # deparse multiple lines of R code
+    x <- enexpr(x)
+    assert_call(x, call_name = "{")
+    as_glue(exprs_deparse(call_args(x)))
+    # Returns: [character]
+}
--- dev/DEV.R
+++ dev/DEV.R
@@ -1,17 +1,31 @@
-###Dev Setup -----
-## INSTALL: CTRL + SHIFT + B
-sDevTools::clearEnv() ## CTRL + SHIFT + R
-library(sDevTools)
-library(exprTools)
-loadDependencies()
-loadUtils()
 #Check Usage -----
 checks=checkPackageUsage()
 runTests(package="exprTools")
 #Dismiss Usage Warnings -----
 #  suppressUsageWarnings(checks)
+###Dev Setup -----
+## INSTALL: CTRL + SHIFT + B
+sDevTools::clearEnv() ## CTRL + SHIFT + R
+library(sDevTools)
+library(exprTools)
+loadUtils()
 #Dev -----
-new_fn((),#args
- desc=  ,#Function description
- return=  #Function returns
- )
+expr_deparse_lines<-
+ function(x){
+   #Documentation
+   fdoc("deparse multiple lines of R code","[character]")
+   #Assertions
+   x<-enexpr(x)
+   assert_call(x, call_name = "{")
+   #TO DO
+   as_glue(exprs_deparse(call_args(x)))
+ }
+#document------
+ fn_document(expr_deparse_lines,{
+expr_deparse_lines({
+  sq<-function(x){
+    x^2
+    }
+  sq(2)
+  sq(4)
+})})
