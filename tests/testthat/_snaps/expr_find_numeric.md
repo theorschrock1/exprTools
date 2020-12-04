ad4eb6bc5d6bbdf8c6716f180e2ad1b3789051ed
theordore schrock <theorschrock@gmail.com>
2020-12-04 16:43:40 -0700

    added tests for "expr_find_numeric"

---
+++ tests/testthat/_snaps/expr_find_numeric.md
@@ -0,0 +1,7 @@
+# expr_find_numeric
+
+    Code
+      expr_find_numeric(x)
+    Output
+      [1] 1.00 1.04
+
---
+++ tests/testthat/test_expr_find_numeric.R
@@ -0,0 +1,5 @@
+test_that("expr_find_numeric", {
+  local_edition(3)
+  x = expr(list(1L, T, "a", 1.04, fun("x")))
+  expect_snapshot(expr_find_numeric(x), cran = TRUE)
+})
