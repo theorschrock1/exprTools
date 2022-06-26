e66c46a158f8f44ef1b3b30ecc8d122955f27ed8
theodore schrock <theo@consumerdirect.com>
2022-06-26 10:32:06 -0700

    added check_json.R

---
+++ R/check_json.R
@@ -0,0 +1,31 @@
+#' Check that a string is a valid JSON object.
+
+#' @name check_json
+#' @param x
+#' @param check_output  \code{[call]}
+#' @param null.ok  \code{[logical]}  Must have an exact length of \code{1}.  Defaults to \code{FALSE}
+#' @return \code{check_json}: \code{[TRUE|error]}
+#' @examples
+
+#'  check_json(x = '[1,3]', check_numeric(len = 2))
+#'  check_json(x = '[1,3]', check_numeric(len = 1))
+#'  check_json(x = '{\'one\':[1,3]}', check_named_list(structure = list(one = numeric())))
+#'  check_json(x = '{\'one\':[1,3]}', check_named_list(structure = list(two = numeric())))
+#' @export
+check_json <- function(x, check_output, null.ok = FALSE) {
+    # Check that a string is a valid JSON object
+    check_output <- enexpr(check_output)
+    assert_call(check_output)
+    assert_logical(null.ok, len = 1)
+    if (is.null(x) && null.ok == TRUE)
+        return(TRUE)
+    out <- try(jsonlite::fromJSON(x), silent = T)
+    if (is_error(out))
+        return("Invalid JSON string")
+    check <- expr_modify_fn_args(check_output, new_args = list(x = expr(out)))
+    res = eval(check)
+    if (!isTRUE(res))
+        return(str_replace(res, "Must be", "Must be a JSON string"))
+    return(res)
+    # Returns: \code{[TRUE|error]}
+}
